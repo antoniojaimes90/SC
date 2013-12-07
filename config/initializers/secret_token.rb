14cb27f36e36325b77100d0c0023696005308288
antoniojaimes90 <antoniojaimes90@gmail.com>
2013-12-07 13:59:17 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Smoothchair::Application.config.secret_key_base = '041e54f0c8224465baddd1817a922022e91eabc523212f02526a70053dbcb42fb3371d20ce21d4aa39b5c810430db3c9fea084613f20ffe110a9be60b71b060e'
